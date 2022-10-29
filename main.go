755c7714d69ae5f22254d0056be01c55d4a59427
wangxuncheng <wangxc@goldensecurity.com.cn>
2022-10-29 17:25:44 +0800

    项目初始化

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"GinShopDemo/controller"
+	"GinShopDemo/tool"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	cfg, err := tool.ParseConfig("./config/app.json")
+	if err != nil {
+		panic(err.Error())
+	}
+	app := gin.Default()
+
+	registerRouter(app)
+	app.Run(cfg.AppHost + ":" + cfg.AppPort)
+}
+
+func registerRouter(router *gin.Engine) {
+	new(controller.HelloController).Router(router)
+}
